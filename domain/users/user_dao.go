3e9fa631bd50c28b5920b18f5400fd517bf7820c
Bhushan Deshmukh <dbhushan912@gmail.com>
2021-03-28 19:17:07 +0530

    inserting rows in sql

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -2,10 +2,16 @@ package users
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/dbhushan9/bookstore_users-api/datasources/mysql/users_db"
 	"github.com/dbhushan9/bookstore_users-api/utils/date_utils"
 	"github.com/dbhushan9/bookstore_users-api/utils/errors"
+)
+
+const (
+	indexUniqueEmail = "email_UNIQUE"
+	queryInsertUser  = "INSERT INTO users(first_name,last_name,email,date_created) VALUES (?, ?, ?, ?)"
 )
 
 var (
@@ -32,16 +38,38 @@ func (user *User) Get() *errors.RestErr {
 }
 
 func (user *User) Save() *errors.RestErr {
-	current := usersDB[user.Id]
-	if current != nil {
-		if current.Email == user.Email {
-			return errors.NewBadRequestError(fmt.Sprintf("email %s already exists", user.Email))
-		}
-		return errors.NewBadRequestError(fmt.Sprintf("user  %d already exists", user.Id))
+	stmt, err := users_db.Client.Prepare(queryInsertUser)
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
 	}
 
+	defer stmt.Close()
+
 	user.DateCreated = date_utils.GetNowString()
-	usersDB[user.Id] = user
+	insertResult, err := stmt.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated)
+	if err != nil {
+		if strings.Contains(err.Error(), indexUniqueEmail) {
+			return errors.NewBadRequestError(fmt.Sprintf("email %s already exists", user.Email))
+		}
+		return errors.NewInternalServerError(fmt.Sprintf("error when trying to save the user: %s", err.Error()))
+	}
+
+	userId, err := insertResult.LastInsertId()
+	if err != nil {
+		return errors.NewInternalServerError(fmt.Sprintf("error when trying to save the user: %s", err.Error()))
+	}
+	user.Id = userId
+
+	// current := usersDB[user.Id]
+	// if current != nil {
+	// 	if current.Email == user.Email {
+	// 		return errors.NewBadRequestError(fmt.Sprintf("email %s already exists", user.Email))
+	// 	}
+	// 	return errors.NewBadRequestError(fmt.Sprintf("user  %d already exists", user.Id))
+	// }
+
+	// user.DateCreated = date_utils.GetNowString()
+	// usersDB[user.Id] = user
 
 	return nil
 }
